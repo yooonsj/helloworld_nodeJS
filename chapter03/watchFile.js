e4250bbc2fb5095fa5f2cd02ca847e4d54720554
yooonsj <yooonsj@gmail.com>
2015-06-28 15:57:29 +0900

    파일 변경사항 감시

---
+++ chapter03/watchFile.js
@@ -0,0 +1,12 @@
+/*
+ * 파일 변경사항 감시
+ */
+var fs = require('fs');
+
+fs.watchFile('./example.txt'
+  , {persistent: true, interval: 0}
+  , function(curr, prev) {
+    console.log('현재 파일의 수정시간 : ' + curr.mtime);
+    console.log('이전 파일의 수정시간 : ' + prev.mtime);
+  }
+);
