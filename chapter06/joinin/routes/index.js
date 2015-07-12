fb8d420d600f5040e32d373fb7f5e726c8266638
yooonsj <yooonsj@gmail.com>
2015-07-12 15:01:35 +0900

    회원가입 폼 생성

--- chapter06/joinin/routes/index.js
+++ chapter06/joinin/routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/join', function(req, res, next) {
+  res.render('join-form', {title: 'Express'});
+});
+
+router.post('/join', function(req, res, next) {
+  res.render('join-result', {
+    username: req.body.name
+    , useremail: req.body.email
+    , title: 'Express'
+
+  })
+})
+
 module.exports = router;
