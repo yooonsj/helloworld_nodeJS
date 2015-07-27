aa1b1d6972f35138eab07719a3b6c0f34f2fbd8f
YoonSangjun <yooonsj@gmail.com>
2015-07-27 10:54:37 +0900

    오타 수정

--- chapter07/connect/server.js
+++ chapter07/connect/server.js
@@ -13,7 +13,7 @@ var server = http.createServer(function(req, res) {
     path.exists(filename, function(exists) {
         if (!exists) {
             res.writeHead(404, {'Content-Type': 'text/plain'});
-            res.write('404 Not Fount\n');
+            res.write('404 Not Found\n');
             res.end();
             return;
         }
